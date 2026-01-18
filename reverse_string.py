b978584bccbfd6bc509cdc23bb49404000ff6837
Sanjota-Nesaragi <01fe24bca239@kletech.ac.in>
2026-01-18 12:44:19 +0530

    Update reverse_string.py

--- reverse_string.py
+++ reverse_string.py
@@ -1,6 +1,12 @@
+import sys
+
 def reverse_text(text):
     return text[::-1]
 
 if __name__ == "__main__":
-    text = input("Enter text: ")
-    print("Reversed:", reverse_text(text))
+    try:
+        text = sys.argv[1]
+    except IndexError:
+        text = input("Enter text: ")
+
+    print(reverse_text(text))
